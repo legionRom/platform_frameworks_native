15ab95030c41f5312723ec8c509205c131152015
rajkale99 <kaleraj.rk@gmail.com>
2019-05-07 16:58:32 +0530

    Revert "input: Adjust priority"
    This reverts commit 97e898b6d68cf5d0b720599707b90ee7eaf3a524.

--- services/surfaceflinger/EventThread.cpp
+++ services/surfaceflinger/EventThread.cpp
@@ -22,7 +22,6 @@
 #include <chrono>
 #include <cstdint>
 
-#include <bfqio/bfqio.h>
 #include <cutils/compiler.h>
 #include <cutils/sched_policy.h>
 
@@ -73,8 +72,6 @@ EventThread::EventThread(VSyncSource* src, ResyncWithRateLimitCallback resyncWit
     }
 
     set_sched_policy(tid, SP_FOREGROUND);
-
-    android_set_rt_ioprio(tid, 1);
 }
 
 EventThread::~EventThread() {
